d0c1fb91500c86f250dca8aca089f5a1ff5d178e
Cody Pin <codypin123@gmail.com>
2023-01-12 00:54:45 +0800

    Fixed list overflow

--- lib/store_screens/store_coupon_list_screen.dart
+++ lib/store_screens/store_coupon_list_screen.dart
@@ -41,110 +41,112 @@ class _StoreCouponListState extends State<StoreCouponListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text(
-            "Your Store's Coupons",
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w600,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text(
+              "Your Store's Coupons",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          FutureBuilder(
-            future: initCoupons,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return RefreshIndicator(
-                  key: _refreshIndicatorKey,
-                  onRefresh: fetchStoreCoupons,
+            FutureBuilder(
+              future: initCoupons,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return RefreshIndicator(
+                    key: _refreshIndicatorKey,
+                    onRefresh: fetchStoreCoupons,
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: coupons.length,
+                      itemBuilder: (context, index) {
+                        final imageURL = client
+                            .getFileUrl(coupons[index],
+                                coupons[index].getStringValue('image'))
+                            .toString();
 
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: coupons.length,
-                    itemBuilder: (context, index) {
-                      final imageURL = client
-                          .getFileUrl(coupons[index],
-                              coupons[index].getStringValue('image'))
-                          .toString();
-
-                      return GestureDetector(
-                        onTap: () {
-                          // go to this coupon's details screen
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => StoreCouponScreen(
-                                coupon: coupons[index],
-                                image: Image.network(
-                                  imageURL,
-                                  errorBuilder: (context, error, stackTrace) {
-                                    return const Icon(
-                                      Icons.card_giftcard,
-                                      size: 100,
-                                    );
-                                  },
+                        return GestureDetector(
+                          onTap: () {
+                            // go to this coupon's details screen
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => StoreCouponScreen(
+                                  coupon: coupons[index],
+                                  image: Image.network(
+                                    imageURL,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return const Icon(
+                                        Icons.card_giftcard,
+                                        size: 100,
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              child: SizedBox(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ConstrainedBox(
+                                      constraints: const BoxConstraints(
+                                        maxHeight: 200.0,
+                                      ),
+                                      child: Hero(
+                                        tag: coupons[index].id,
+                                        child: Image.network(
+                                          imageURL,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return const Icon(Icons.card_giftcard,
+                                                size: 100);
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      coupons[index].getStringValue('name'),
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text(
+                                      coupons[index]
+                                          .getStringValue('description'),
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    // Image.network(imageURL),
+                                  ],
                                 ),
                               ),
                             ),
-                          );
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Card(
-                            child: SizedBox(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  ConstrainedBox(
-                                    constraints: const BoxConstraints(
-                                      maxHeight: 200.0,
-                                    ),
-                                    child: Hero(
-                                      tag: coupons[index].id,
-                                      child: Image.network(
-                                        imageURL,
-                                        errorBuilder:
-                                            (context, error, stackTrace) {
-                                          return const Icon(Icons.card_giftcard,
-                                              size: 100);
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    coupons[index].getStringValue('name'),
-                                    style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  Text(
-                                    coupons[index]
-                                        .getStringValue('description'),
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                  // Image.network(imageURL),
-                                ],
-                              ),
-                            ),
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
-        ],
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
